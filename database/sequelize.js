1690b4ab7597a7e071e293f032bc49837fef03a8
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-09-20 14:30:27 -0300

    Adicionando .env

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.PG_DATABASE,
+   process.env.PG_USER, process.env.PG_PASSWORD, {
+    host: process.env.PG_HOST,
     dialect: 'postgres'
 });
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -5,4 +6,6 @@ app.use(express.json());
 const usuarioRouter = require('./routes/UsuarioRouter');
 app.use('/usuarios', usuarioRouter);
 
-app.listen(3000);
+app.listen(process.env.API_PORT, () => {
+    console.log(`Servidor rodando na porta ${process.env.API_PORT}`);
+});
