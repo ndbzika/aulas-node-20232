d8655205fb7e935502380f4d83cd6d2e21959f27
Paulo Freitas <paulo.freitas.nt@gmail.com>
2023-09-20 16:10:05 -0300

    Adicionando cors e exemplo com HTML

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 app.use(express.json());
+const cors = require('cors');
+app.use(cors());
 
 const usuarioRouter = require('./routes/UsuarioRouter');
 app.use('/usuarios', usuarioRouter);
